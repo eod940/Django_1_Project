370488a3fd5644821f35ac6ee28d07e9e0fa6cca
eod940 <eod940@gmail.com>
2020-12-26 00:27:00 +0900

    blog app - Post:

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    # title: blog title
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    
+    #created : when
+    created = models.DateTimeField()
+    #만약 다른 유저가 글을올리고 계정을 삭제하면 글도 삭제 = True
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
