2d40b000aa4d6bc51bd9d072b677bae979fa2b97
eod940 <eod940@gmail.com>
2020-12-26 18:26:58 +0900

    Post: list view

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 # Create your views here.
+# 어떠한 model을 template에 담아주는 방식 사용 => FBV -> MBV
 
-def index(request):
-    posts = Post.objects.all()
+class PostList(ListView):
+    model = Post
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+# def index(request):
+#     posts = Post.objects.all()
+
+    # return render(
+    #     request,
+    #     'blog/index.html',
+    #     {
+    #         'posts': posts,
+    #     }
+    # )
