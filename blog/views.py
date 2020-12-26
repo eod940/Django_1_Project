3962c661026ad81b6d4699446b1c4f3557de7d0d
eod940 <eod940@gmail.com>
2020-12-27 01:19:18 +0900

    blog: bootswatch in static

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView
 # Create your views here.
-# 어떠한 model을 template에 담아주는 방식 사용 => FBV -> MBV
+# 어떠한 model을 template에 담아주는 방식 사용 => FBV(function based view) -> CBV(class based view)
 
 class PostList(ListView):
     model = Post
 
     def get_queryset(self):
+        # 최신순으로 정렬
         return Post.objects.order_by('-created')
 
 # def index(request):
