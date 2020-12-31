cea7f7c6407a903e8c53713440f064ee49872919
eod940 <eod940@gmail.com>
2020-12-31 10:58:22 +0900

    make PostDetail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 # 어떠한 model을 template에 담아주는 방식 사용 => FBV(function based view) -> CBV(class based view)
 
@@ -11,6 +11,19 @@ class PostList(ListView):
         # 최신순으로 정렬
         return Post.objects.order_by('-created')
 
+class PostDetail(DetailView):
+    model = Post
+
+# def post_detail(request, pk):
+#     blog_post = Post.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'blog_post':blog_post,
+#         }
+#     )
 # def index(request):
 #     posts = Post.objects.all()
 
