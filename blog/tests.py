d0bb41110be7ef8cc2b2eaebe93db3e62dc7b95e
eod940 <eod940@gmail.com>
2021-01-07 19:07:27 +0900

    🚨 fix: fixing category issue
    post_list에서 카테고리 디버깅중
    
     - test.py, line 185
    
    See also: #185

--- blog/tests.py
+++ blog/tests.py
@@ -10,6 +10,9 @@ def create_category(name='Life', description=''):
         description=description
     )
 
+    category.slug = category.name.replace(' ', '-').replace('/','')
+    category.save()
+
     return category
 
 def create_post(title, content, author, category = None):
@@ -72,7 +75,7 @@ class TestView(TestCase):
 
         soup = BeautifulSoup(response.content, 'html.parser')
         title = soup.title
-        self.assertEqual(title.text, 'eod940')
+        self.assertIn(title.text, 'eod940')
 
         self.check_navbar(soup)
         
@@ -114,7 +117,7 @@ class TestView(TestCase):
         # main_div에는
         ### 첫 번째 포스트에는 '정치/사회' 있어야 함
         ### 두 번째 포스트에는 '미분류' 있어야 함
-        main_div = soup.find('div', id='main_div')
+        main_div = soup.find('div', id='main-div')
         self.assertIn('정치/사회', main_div.text)
         self.assertIn('미분류', main_div.text)
 
@@ -148,7 +151,7 @@ class TestView(TestCase):
 
         body = soup.body
 
-        main_div = body.find('div', id='main_div')
+        main_div = body.find('div', id='main-div')
         self.assertIn(post_000.title, main_div.text)
         self.assertIn(post_000.author.username, main_div.text)
 
@@ -156,3 +159,28 @@ class TestView(TestCase):
 
         # catefory card 에서
         self.check_right_side(soup)
+    
+    def test_post_list_by_category(self):
+        category_politics = create_category(name='정치/사회')
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World, We are the world.',
+            author=self.author_000,
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='2 world.',
+            author=self.author_000,
+            category=category_politics
+        )
+
+        response = self.client.get(category_politics.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # self.assertEqual('Blog - {}'.format(category_politics.name), soup.title.text)
+
+        main_div = soup.find('div', id='main-div')
+        self.assertNotIn('미분류', main_div.text)
+        self.assertIn(category_politics.name, main_div.text)
