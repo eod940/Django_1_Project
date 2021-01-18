4a255cbe25eb3f1ab55c9822bacbfa0719b71829
eod940 <eod940@gmail.com>
2021-01-18 23:55:09 +0900

    ⚡️feat: complete comment feat
     코멘트에 edit, delete 버튼이 생겼습니다.

--- blog/tests.py
+++ blog/tests.py
@@ -212,6 +212,7 @@ class TestView(TestCase):
         )
 
         comment_000 = create_comment(post_000, text='a test comment', author=self.user_obama)
+        comment_001 = create_comment(post_000, text='a test comment', author=self.author_000)
 
         tag_america = create_tag(name='america')
         post_000.tags.add(tag_america)
@@ -282,6 +283,15 @@ class TestView(TestCase):
         #    로그인 한 사용자와 post.author이 동일하면 EDIT 버튼이 있다.
         self.assertEqual(post_000.author, self.author_000)
         self.assertNotIn('EDIT', main_div.text)
+
+        comments_div = main_div.find('div', id='comment-list')
+        comment_000_div = comments_div.find('div', id='comment-id-{}'.format(comment_000.pk))
+        self.assertIn('edit',comment_000_div.text)
+        self.assertIn('delete',comment_000_div.text)
+
+        comment_001_div = comments_div.find('div', id='comment-id-{}'.format(comment_001.pk))
+        self.assertNotIn('edit',comment_001_div.text)
+        self.assertNotIn('delete',comment_001_div.text)
         
     
     def test_post_list_by_category(self):
