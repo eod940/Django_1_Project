14ad4de9454183c16f19747c3648213ae48ff1da
eod940 <eod940@gmail.com>
2021-01-21 16:51:20 +0900

    🔅feat: google login view
      구글로그인 창이 보입니다.

--- my_site_prj/urls.py
+++ my_site_prj/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('markdownx/', include('markdownx.urls')),
     path('accounts/', include('allauth.urls')),
     # 위의 식 또는 url(r'^markdownx/', include('markdownx.urls')),
+    path('', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
